eadff18450f0f649ab5f99da60e188b3654baccf
Spotlight <spotlight@joscomputing.space>
2020-06-26 18:30:51 -0500

    Initial Commit

---
+++ Shared/ScreenshottrApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshottrApp.swift
+//  Shared
+//
+//  Created by Spotlight Deveaux on 6/26/20.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshottrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
